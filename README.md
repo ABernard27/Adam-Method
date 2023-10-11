d8e7930a2719edc29e54dd3031d9f13c9c97b956
ABernard27 <anne.bernard@etu.umontpellier.fr>
2023-10-11 17:03:17 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ADAM-Method
+## ADAM
+
+In this repository, we'll put our research and code on the ADAM method 
+thanks to the article : [ADAM: A METHOD FOR STOCHASTIC 
+OPTIMIZATION](https://browse.arxiv.org/pdf/1412.6980.pdf)
+
+## Author
+CAPEL Alexandre & BERNARD Anne, students at University of Montpellier
+
+## ADAM
+
+ADAM is a method for efficient stochastic optimization. 
+
+Lors d'une descente de gradient, il est parfois difficile de trouver le 
+bon taux d'apprentissage. En effet, la plupart du temps ce taux est fixe 
+et s'il est trop petit cela peut poser des difficultés à trouver le 
+minimum puisque la méthode n'avancera pas assez et elle peut s'arrêter 
+avant de l'atteindre. S'il est trop grand nous avancerons vite mais il est 
+possible de tourner autour du point sans l'atteindre car le pas est trop 
+grand. Il faut donc trouver le pas parfait afin de trouver le minimum. 
+
---
+++ script.py
@@ -0,0 +1,79 @@
+# %%
+# implementation de la méthode ADAM
+
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+# %%
+
+# Instantiate the MNIST dataset
+dataset = MNIST(root='./data', train=True, download=True)
+dataset.transform = ToTensor()
+
+# Extraire les caractéristiques et les étiquettes
+data = DataLoader(dataset, batch_size=128, shuffle=True)
+
+# %%
+
+
+# Define a simple model
+# class SimpleModel(nn.Module):
+#     def __init__(self):
+#         super(SimpleModel, self).__init__()
+#         self.linear = nn.Linear(28*28, 1)
+#         self.logit = nn.Sigmoid()
+
+#     def forward(self, x):
+#         x = self.linear(x)
+#         return self.logit(x)
+
+class LogisticRegressionModel(nn.Module):
+    def __init__(self):
+        super(LogisticRegressionModel, self).__init__()
+        self.linear = nn.Linear(28*28, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        logits = self.linear(x)
+        predictions = self.sigmoid(logits)
+        return predictions
+
+
+# Instantiate the model
+model = LogisticRegressionModel()
+
+
+# Instantiate the Adam optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+loss = nn.NLLLoss()
+stock = []
+
+# Training loop
+for epoch in range(10):
+    for batch, (X, y) in enumerate(data):
+        # Calculate the model predictions
+        output = model(X.view(-1, 28*28))
+
+        # Calculate the loss with logistic regression
+        res = loss(output.squeeze(), y)
+
+        # Reset the gradients
+        optimizer.zero_grad()
+
+        # Backpropagation and weight update
+        res.backward()
+        optimizer.step()
+
+        # Stockage of the loss
+        stock.append(res.item())
+
+# %%
+
+plt.plot(stock)
+plt.show()
+
+# %%
